8943ea81ceefdcfc64a86b30fce474df3ccb9aa1
Tarek Mahmud <72188853+mahmud4020@users.noreply.github.com>
2023-07-12 02:16:19 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import {View, Text} from 'react-native';
+
+import StackNav from './src/navigator/StackNav';
+
+function App() {
+  return <StackNav />;
+}
+
+export default App;
